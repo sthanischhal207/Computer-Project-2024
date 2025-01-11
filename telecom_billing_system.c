e2e3ebe8419933c74aefbbbd99d8916e818052b6
sthanischhal207 <sthanischhal207@gmail.com>
2025-01-11 09:26:29 +0545

    Create telecom_billing_system.c

---
+++ telecom_billing_system.c
@@ -0,0 +1,6 @@
+#include <stdio.h>		// Standard I/O library for basic input and output operations
+#include <stdlib.h>		// Standard library for dynamic memory allocation and utility functions
+#include <string.h>		// String library for operations like strcmp, strcpy, etc.
+#include <ctype.h>		// Character type library for functions like isdigit, isspace
+#include <math.h>		// Math library for mathematical operations like pow
+#include "for_all.h"		// Custom header file with utility functions and declarations
